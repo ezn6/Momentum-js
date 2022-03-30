21d615a1eec2739e7966523ebb40870d2036e75f
ezn6 <kcourtney@naver.com>
2022-03-30 23:11:26 +0900

    saving, loading todos

--- js/todo.js
+++ js/todo.js
@@ -2,20 +2,53 @@ const todoForm = document.querySelector('#todo-form');
 const todoInput = document.querySelector('#todo-form input');
 const todoList = document.querySelector('#todo-list');
 
-function wirteTodoSubmit(event) {
-  event.preventDefault();
+const TODOS_KEY = 'todo';
+let todos = [];
+
+function saveTodo() {
+  localStorage.setItem(TODOS_KEY, JSON.stringify(todos));
+}
+
+function paintTodo(newTodo) {
   const li = document.createElement('li');
+  li.id = newTodo.id;
+  // li.className = newTodo.id;
   const span = document.createElement('span');
-  span.innerText = todoInput.value;
+  span.innerText = newTodo.text;
   const button = document.createElement('button');
   li.appendChild(span);
   li.appendChild(button);
   todoList.appendChild(li);
   button.innerText = '❌';
-  todoInput.value = '';
   button.addEventListener('click', () => {
     todoList.removeChild(li);
+    console.log(li.id);
+    todos = todos.filter((i) => i.id !== li.id);
+    console.log(todos);
+    // saveTodo();
   });
 }
 
-todoForm.addEventListener('submit', wirteTodoSubmit);
+function handleTodoSubmit(event) {
+  event.preventDefault();
+  const newTodo = todoInput.value;
+  const newTodoObj = {
+    text: newTodo,
+    id: Date.now(),
+  };
+  paintTodo(newTodoObj);
+  //
+  todos.push(newTodoObj);
+  saveTodo();
+  //
+  todoInput.value = '';
+}
+
+todoForm.addEventListener('submit', handleTodoSubmit);
+
+const savedTodos = localStorage.getItem(TODOS_KEY);
+if (savedTodos !== null) {
+  const parsedTodos = JSON.parse(savedTodos);
+  todos = parsedTodos;
+  parsedTodos.forEach((i) => paintTodo(i));
+}
